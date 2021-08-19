6aebf4795e8c458d0f6493669be34c6e18762968
Sumit Sharma <sumit.16noida@gmail.com>
2021-08-19 21:25:02 +0530

    Add Validation to the user Register Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET /api/users
-// @desc    Test Route
+// @route   POST /api/users
+// @desc    Register a User
 // @access  Public
-router.get('/', (req, res) => {
-  res.send('User Route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is Required').not().isEmpty(),
+    check('email', 'Please Enter a Valid Email').isEmail(),
+    check(
+      'password',
+      'Please Enter a Password with 6 or more Characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User Route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init MiddleWare or BodyParser
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API RUNNING'));
 
 // Define Routes
