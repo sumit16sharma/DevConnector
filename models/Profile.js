e6f89e593ed6fd56fc7811e181f0af394bc64979
Sumit Sharma <sumit.16noida@gmail.com>
2021-08-23 21:48:51 +0530

    Get logged in user profile Route

--- models/Profile.js
+++ models/Profile.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
   user: {
-    user: mongoose.Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
   },
   company: {
